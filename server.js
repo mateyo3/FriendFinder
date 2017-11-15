fd738b8d4c323aaed67fe1c30fe3856db9bd703a
mateyo3 <mateoreyes720@gmail.com>
2017-11-14 18:19:06 -0800

    connectgit add .git add .

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 4545
 
+
+app.get("/", function(req, res) {
+  res.send("Welcome to the Star Wars Page!");
+});
+
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
