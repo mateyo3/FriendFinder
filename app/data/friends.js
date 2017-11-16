34ca5ef3e0287c59b80a52c4a96b9ddb072c0305
mateyo3 <mateoreyes720@gmail.com>
2017-11-15 22:53:01 -0800

    added dummy data for matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,36 @@
 [
 	{
-		"name": "Ninja",
-		"photo": "https://..."
+		"name": "Naruto",
+		"photo": "https://pre00.deviantart.net/b59c/th/pre/f/2016/104/4/f/chibi_naruto_shippuden_by_marcinha20-d9yxwvl.png"
+		"scores": [
+			"5",
+			"1"
+		]
+	},
+	{
+		"name": "Sasuke",
+		"photo": "https://pre00.deviantart.net/956e/th/pre/f/2015/312/e/7/chibi_sasuke_uchiha_sharingan_by_marcinha20-d9g0dce.png"
+		"scores": [
+			"5",
+			"1"
+		]
+	},
+		{
+		"name": "Hinata",
+		"photo": "https://pre00.deviantart.net/0d56/th/pre/f/2016/223/b/2/chibi___hinata_the_last_by_marcinha20-d87eraj.png"
+		"scores": [
+			"5",
+			"1"
+		]
+	},
+		},
+		{
+		"name": "Sakura",
+		"photo": "https://pre00.deviantart.net/a0f8/th/pre/f/2016/311/9/f/chibi_sakura_movie_1_by_marcinha20-danoahm.png"
 		"scores": [
 			"5",
 			"1"
 		]
 	}
+
 ]
