7c682b93c4d881e3fe9d9722362790a2cf485728
mateyo3 <mateoreyes720@gmail.com>
2017-11-19 17:50:22 -0800

    push drop down menu values into newFriend object as array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,36 +3,36 @@ var ninjas = [
 		name: "Naruto",
 		photo: "https://pre00.deviantart.net/b59c/th/pre/f/2016/104/4/f/chibi_naruto_shippuden_by_marcinha20-d9yxwvl.png",
 		scores: [
-			"1",
-			"1",
-			"1"
+			1,
+			1,
+			1
 		]
 	},
 	{
 		name: "Sasuke",
 		photo: "https://pre00.deviantart.net/956e/th/pre/f/2015/312/e/7/chibi_sasuke_uchiha_sharingan_by_marcinha20-d9g0dce.png",
 		scores: [
-			"5",
-			"1",
-			"5"
+			5,
+			1,
+			5
 		]
 	},
 		{
 		name: "Hinata",
 		photo: "https://pre00.deviantart.net/0d56/th/pre/f/2016/223/b/2/chibi___hinata_the_last_by_marcinha20-d87eraj.png",
 		scores: [
-			"5",
-			"5",
-			"5"
+			5,
+			5,
+			5
 		]
 	},
 		{
 		name: "Sakura",
 		photo: "https://pre00.deviantart.net/a0f8/th/pre/f/2016/311/9/f/chibi_sakura_movie_1_by_marcinha20-danoahm.png",
 		scores: [
-			"4",
-			"3",
-			"4"
+			4,
+			3,
+			4
 		]
 	}
 
